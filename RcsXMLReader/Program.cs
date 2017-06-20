3edf8707bc91b834fbebff8ba740796c816bb5cf
Ams <adriansims62781@gmail.com>
2017-06-20 21:40:32 +0100

    More Corrections to linq

--- RcsXMLReader/Program.cs
+++ RcsXMLReader/Program.cs
@@ -20,86 +20,43 @@ namespace RcsXMLReader
                 }
                 var filename = args[0];
                 var doc = XDocument.Load(filename);
-                //var l = (from frecord in doc.Descendants("F")
-                //         let Route = frecord.Attribute("r")?.Value
-                //         select new
-                //         {
-                //             Key = frecord.Attribute("o")?.Value + frecord.Attribute("d")?.Value,
-                //             RouteToTicketList = new
-                //             { Route,
-                //                 TicketList = (from ticket in frecord.Elements("T")
-                //                               select new {
-                //                                   TicketCode = ticket.Attribute("T")?.Value,
-                //                                   FFList = from ff in ticket.Elements("FF")
-                //                                            select new RCSFF
-                //                                            {
-                //                                                StartDate = ff.Attribute("u")?.Value,
-                //                                                EndDate = ff.Attribute("f")?.Value,
-                //                                                SeasonIndicator = ff.Attribute("s")?.Value,
-                //                                                QuoteDate = ff.Attribute("p")?.Value,
-                //                                                Key = ff.Attribute("k")?.Value
-                //                                            }
-                //                               }
-                //                )
-                //             }
-                //         }
-                //);
-
-                //  var l1 = l.ToLookup(x => x.Key, x=>x.TicketList);
-
 
                 var l1 = doc.Descendants("F")
                     .GroupBy(x => x.Attribute("o").Value + x.Attribute("d").Value)
                     .ToLookup(a => a.Key,   // key is flow
                               a => a.GroupBy(b => b.Attribute("r").Value)
                                   .ToLookup(g => g.Key,   // key is route
-                                            g => g.GroupBy(c=>c.Element("T").Attribute("t").Value)
-                                                    .ToLookup(m=>m.Key,  // key is ticketcode
-                                                              m=>m.Elements("T").Select(f=>f.Element("FF"))
-                                                                  .Select(ff=>new RCSFF {
-                                                                                EndDate = ff.Attribute("u").Value,
-                                                                                StartDate = ff.Attribute("f").Value,
-                                                                                QuoteDate = ff.Attribute("p")?.Value,
-                                                                                SeasonIndicator = ff.Attribute("s")?.Value,
-                                                                                Key = ff.Attribute("k")?.Value,
-                                                                            }).ToList())));
+                                            g => g.Elements("T").GroupBy(c => c.Attribute("t").Value)
+                                                    .ToDictionary(m => m.Key,  // key is ticketcode
+                                                              m => m.Elements("FF").Select(o => new
+                                                              {
+                                                                  StartDate = o.Attribute("f").Value,
+                                                                  EndDate = o.Attribute("u").Value,
+                                                                  SeasonIndicator = o.Attribute("s")?.Value,
+                                                                  QuoteDate = o.Attribute("p")?.Value,
+                                                                  Key = o.Attribute("k").Value
+                                                              }
+                                                              ).ToList())));
+                var origin = "8681";
+                var destination = "9998";
+                var l2 = l1[origin + destination];
 
-                var l2 = doc.Descendants("F")
-                    .GroupBy(x => x.Attribute("o").Value + x.Attribute("d").Value)
-                    .ToLookup(a => a.Key,                                           
-                              a => a.Select(b => b.Attribute("r").Value));
-
-                var routes = l1["90139998"];
-                foreach (var route in routes.SelectMany(x=>x))
+                foreach (var route in l2.SelectMany(x=>x))
                 {
-                    Console.WriteLine($"{route.Key}");
-                    foreach (var ticket in route.SelectMany(x => x))
+                    Console.WriteLine($"ROUTE: {route.Key}");
+                    foreach (var ticket in route.SelectMany(x=>x))
                     {
-                        Console.WriteLine($"ticket: {ticket.Key}");
-                        foreach (var ff in ticket.SelectMany(x => x))
+                        Console.WriteLine($"ticket is {ticket.Key}");
+                        var fflist = ticket.Value;
+                        foreach (var ff in fflist)
                         {
-                            Console.WriteLine($"start: {ff.StartDate} end: {ff.EndDate} key: {ff.Key}");
+                            var season = ff.SeasonIndicator == null ? "" : $" season {ff.SeasonIndicator} ";
+                            var quote = ff.QuoteDate == null ? "" : $" quote {ff.QuoteDate} ";
+                            Console.WriteLine($"start {ff.StartDate} end {ff.EndDate}" + season + quote +  $"key {ff.Key}");
                         }
                     }
+                    Console.WriteLine("----------------");
                 }
-
-                foreach (var flow in l1)
-                {
-                    Console.WriteLine($"{flow.Key}");
-                    foreach (var route in flow.SelectMany(x=>x))
-                    {
-                        Console.WriteLine($"route key is {route.Key}");
-                        foreach (var ticket in route.SelectMany(x=>x))
-                        {
-                            Console.WriteLine($"ticket key is {ticket.Key}");
-                            foreach (var ff in ticket.SelectMany(x=>x))
-                            {
-                                Console.WriteLine($"");
-                            }
-                        }
-                    }
-                }
-
                 Console.WriteLine("");
             }
             catch (Exception ex)

9377ea2f44449d83c64ba197d944913ca18a3fbc
Adrian Sims <asims@parkeon.com>
2017-06-20 18:29:13 +0100

    Small mods

--- RcsXMLReader/Program.cs
+++ RcsXMLReader/Program.cs
@@ -50,39 +50,51 @@ namespace RcsXMLReader
 
                 var l1 = doc.Descendants("F")
                     .GroupBy(x => x.Attribute("o").Value + x.Attribute("d").Value)
-                    .ToLookup(a => a.Key,                                           // flow
+                    .ToLookup(a => a.Key,   // key is flow
                               a => a.GroupBy(b => b.Attribute("r").Value)
-                                  .ToLookup(g => g.Key,                             // route
+                                  .ToLookup(g => g.Key,   // key is route
                                             g => g.GroupBy(c=>c.Element("T").Attribute("t").Value)
-                                                    .ToLookup(m=>m.Key,             // ticket code
-                                                              m=>m.Elements("T").Select(f=>f.Element("FF")).Select(ff=>new RCSFF { EndDate = ff.Attribute("u").Value}).ToList())));
+                                                    .ToLookup(m=>m.Key,  // key is ticketcode
+                                                              m=>m.Elements("T").Select(f=>f.Element("FF"))
+                                                                  .Select(ff=>new RCSFF {
+                                                                                EndDate = ff.Attribute("u").Value,
+                                                                                StartDate = ff.Attribute("f").Value,
+                                                                                QuoteDate = ff.Attribute("p")?.Value,
+                                                                                SeasonIndicator = ff.Attribute("s")?.Value,
+                                                                                Key = ff.Attribute("k")?.Value,
+                                                                            }).ToList())));
 
                 var l2 = doc.Descendants("F")
                     .GroupBy(x => x.Attribute("o").Value + x.Attribute("d").Value)
-                    .ToLookup(a => a.Key,                                           // flow
+                    .ToLookup(a => a.Key,                                           
                               a => a.Select(b => b.Attribute("r").Value));
 
-                var r = l1["09909998"];
-                var r2 = l2["09909998"];
+                var routes = l1["90139998"];
+                foreach (var route in routes.SelectMany(x=>x))
+                {
+                    Console.WriteLine($"{route.Key}");
+                    foreach (var ticket in route.SelectMany(x => x))
+                    {
+                        Console.WriteLine($"ticket: {ticket.Key}");
+                        foreach (var ff in ticket.SelectMany(x => x))
+                        {
+                            Console.WriteLine($"start: {ff.StartDate} end: {ff.EndDate} key: {ff.Key}");
+                        }
+                    }
+                }
 
                 foreach (var flow in l1)
                 {
                     Console.WriteLine($"{flow.Key}");
-                    foreach (var routelookup in flow)
+                    foreach (var route in flow.SelectMany(x=>x))
                     {
-                        foreach (var route in routelookup)
+                        Console.WriteLine($"route key is {route.Key}");
+                        foreach (var ticket in route.SelectMany(x=>x))
                         {
-                            Console.WriteLine($"route key is {route.Key}");
-                            foreach (var ticketlookup in route)
+                            Console.WriteLine($"ticket key is {ticket.Key}");
+                            foreach (var ff in ticket.SelectMany(x=>x))
                             {
-                                foreach (var ticket in ticketlookup)
-                                {
-                                    Console.WriteLine($"{ticket.Key}");
-                                    foreach (var fflookup in ticket)
-                                    {
-                                        Console.WriteLine($"");
-                                    }
-                                }
+                                Console.WriteLine($"");
                             }
                         }
                     }
